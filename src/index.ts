f33e1394447eb60658bbf9aff7033adab3e2a8fc
urmaul <me@urmaul.com>
2022-12-02 22:40:02 +0100

    Generate auth URL

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 'use strict';
 
+// TODO: Do we need sitka
 import { Logger } from 'sitka';
+import generator, { Pleroma } from 'megalodon'
 
+// TODO: Delete it
 export class Example {
 	/* Private Instance Fields */
 
@@ -20,3 +23,19 @@ export class Example {
 		return param;
 	}
 }
+
+// TODO: panic on missing env variables
+const baseUrl: string = process.env.FEDIVERSE_BASE_URL || ''
+const clientId: string = process.env.FEDIVERSE_CLIENT_ID || ''
+const clientSecret: string = process.env.FEDIVERSE_CLIENT_SECRET || ''
+
+// TODO: Use app config type
+console.log('Got config', {baseUrl, clientId, clientSecret})
+
+// TODO: don't assume client type
+const client: Pleroma = generator('pleroma', baseUrl) as Pleroma
+
+client.generateAuthUrl(clientId, clientSecret, {scope: ['read']})
+	.then(url => console.log('Got url', url))
+
+// TODO: accept auth code
